92f6d1656e9827604747f4b23355a44f022496bd
alec-tschantz <tschantz.alec@gmail.com>
2020-07-11 19:22:05 +0100

    version 0.2

--- script.py
+++ script.py
@@ -19,11 +19,11 @@ def main(cf):
     label_train = mnist_utils.get_labels(train_set)
     label_test = mnist_utils.get_labels(test_set)
 
-    if cf.datasize is not None:
-        img_train = img_train[:, 0:cf.datasize]
-        img_test = img_test[:, 0:cf.datasize]
-        label_train = label_train[:, 0:cf.datasize]
-        label_test = label_test[:, 0:cf.datasize]
+    if cf.data_size is not None:
+        img_train = img_train[:, 0:cf.data_size]
+        img_test = img_test[:, 0:cf.data_size]
+        label_train = label_train[:, 0:cf.data_size]
+        label_test = label_test[:, 0:cf.data_size]
 
     msg = "img_train {} img_test {} label_train {} label_test {}"
     print(msg.format(img_train.shape, img_test.shape, label_train.shape, label_test.shape))
@@ -43,7 +43,7 @@ def main(cf):
 
     print("starting training...")
     for epoch in range(cf.n_epochs):
-        print(f"epoch: {epoch}")
+        print(f"\nepoch: {epoch}")
         model.train_epoch(img_train, label_train)
         model.test(img_test, label_test)
 
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 
     cf.n_epochs = 100
     cf.batch_size = 20
-    cf.datasize = 1000
+    cf.data_size = 1000
 
     cf.neurons = [784, 500, 500, 10]
     cf.n_layers = len(cf.neurons)
